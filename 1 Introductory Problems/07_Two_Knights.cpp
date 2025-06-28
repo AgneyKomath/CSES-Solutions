ca699065a2c29e2ab9b4a1e4ae7ff79c230627ed
Agney Komath <agneybiju@gmail.com>
2025-06-29 03:48:47 +0530

    Initial Commit 29-06-25

--- 1 Introductory Problems/07_Two_Knights.cpp
+++ 1 Introductory Problems/07_Two_Knights.cpp
@@ -9,17 +9,9 @@ int main(void){
     cin>>n;
 
     for(int k = 1; k<=n; k++){
-        int combinations = (k * k) * (k * k - 1) / 2;
-
-        int bad = 0;
-        if(n>=3) bad += 4*2 + ; // corners
-        if(n>=4) bad += 
-
-
-        int ans = ((i*i)*(i*i-1))/2 -4*(i-1)*(i-2);
-        cout<<ans<<'\n';
+        long long res = (1ll * (k * k) * (k * k - 1)) / 2 - 4ll * (k - 1) * (k - 2);
+        cout<<res<<'\n';
     }
 
-    
     return 0;
 }
