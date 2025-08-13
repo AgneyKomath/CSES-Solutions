a67616eeb06e63378c4f12841e4e2bd90fc4abac
Agney Komath <agneybiju@gmail.com>
2025-08-13 23:41:30 +0530

    Range Queries Updated

--- 05 Range Queries/07_Forest_Queries.cpp
+++ 05 Range Queries/07_Forest_Queries.cpp
@@ -4,10 +4,6 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    #ifdef Fusion15
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
 
     int n, q;
     cin>>n>>q;
--- 05 Range Queries/09_List_Removals.cpp
+++ 05 Range Queries/09_List_Removals.cpp
@@ -100,10 +100,6 @@ int main(){
 // int main(){
 //     ios::sync_with_stdio(false);
 //     cin.tie(NULL);
-//     #ifdef Fusion15
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt", "w", stdout);
-//     #endif
 
 //     int n;
 //     cin>>n;
