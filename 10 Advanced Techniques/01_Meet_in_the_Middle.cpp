88f2a2301d14c1b98b6ec959eb0436ae66bd4f9f
Agney Komath <agneybiju@gmail.com>
2025-08-21 19:55:15 +0530

    Advanced Techniques Updates

--- 10 Advanced Techniques/01_Meet_in_the_Middle.cpp
+++ 10 Advanced Techniques/01_Meet_in_the_Middle.cpp
@@ -1,50 +1,40 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 
-int32_t main(){
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    #ifdef Fusion15
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
 
     int n, x;
     cin>>n>>x;
-
+    
     vector<int> a(n);
     for(int &i:a) cin>>i;
 
-
-    auto getSums =[&](int l, int r){
-        vector<int> res;
-        int n = r-l+1;
-        
-        for(int i = 0;i<(1<<n); i++){
-            int sum = 0;
-            for(int j = 0;j<n; j++){
-                if(i & (1<<j)) sum += a[l+j];
+    auto subsetSum = [&](int l, int r){
+        int sz = r-l+1;
+        int len = 1<<sz;
+        vector<long long> res(len, 0);
+        for(int i = 0; i<len; i++){
+            for(int j = 0; j<sz; j++){
+                if(i&(1<<j)) res[i] += a[j+l];
             }
-            res.push_back(sum);
         }
         sort(res.begin(), res.end());
         return res;
     };
+    
+    vector<long long> left = subsetSum(0, (n-1)/2);
+    vector<long long> right = subsetSum((n+1)/2, n-1);
 
-    vector<int> left = getSums(0, (n-1)/2);
-    vector<int> right = getSums((n+1)/2, n-1);
-
-    int res = 0;
-
-    for(int i:left){
-        auto lb = lower_bound(right.begin(), right.end(), x-i);
-        auto ub = upper_bound(right.begin(), right.end(), x-i);
+    long long res = 0;
+    for(auto i:left){
+        int lb = lower_bound(right.begin(), right.end(), x-i) - right.begin();
+        int ub = upper_bound(right.begin(), right.end(), x-i) - right.begin();
         res += ub-lb;
     }
 
     cout<<res;
-    
     
     return 0;
 }
--- 10 Advanced Techniques/02_Hamming_Distance.cpp
+++ 10 Advanced Techniques/02_Hamming_Distance.cpp
@@ -1,37 +1,38 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 
-int32_t main(){
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    #ifdef Fusion15
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-
+    
     int n, k;
     cin>>n>>k;
+    
+    auto stringToInt=[&](string &s){
+        int res = 0;
+        for(int i = 0; i<k; i++){
+            if(s[i]=='1'){
+                res |= (1<<i);
+            }
+        }
+        return res;
+    };
 
-    vector<int>a(n);
-    for(int i = 0;i<n;i++){
+    vector<int> a(n);
+    for(int i = 0; i<n; i++){
         string s;
         cin>>s;
-        int sum = 0;
-        for(int j = 0;j<k;j++){
-            if(s[j]=='1') sum += (1<<(k-j-1));
-        }
-        a[i] = sum;
+        a[i] = stringToInt(s);
     }
 
     int res = k;
-    for(int i = 0;i<n;i++){
-        for(int j = i+1;j<n;j++){
-            int v = __builtin_popcount(a[i]^a[j]);
-            res = min(res, v);
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j<n; j++){
+            int d = __builtin_popcount(a[i]^a[j]);
+            res = min(res, d);
         }
     }
-    cout<<res<<'\n';
+    cout<<res;
     
     return 0;
 }
