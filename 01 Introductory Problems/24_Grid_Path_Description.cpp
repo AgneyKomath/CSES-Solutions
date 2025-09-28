d2fc84d3854de3b25169fcf827dd948da26b1673
Agney Komath <agneybiju@gmail.com>
2025-09-28 16:46:15 +0530

    Grid Path Optimization

--- 01 Introductory Problems/24_Grid_Path_Description.cpp
+++ 01 Introductory Problems/24_Grid_Path_Description.cpp
@@ -1,47 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 0.98s on C++ 20 lmaooo
-
 // DLUR
-int dir[] = {1, 0, -1, 0, 1};
-string dirs = "DLUR";
+static const int dir[] = {1, 0, -1, 0, 1};
+static const string dirs = "DLUR";
 
 static constexpr int n = 7;
-int vis[n][n]{};
+static long long vis = 0;
 
-bool isBorder(int r, int c){
-    return r>=n || r<0 || c>=n || c<0 || vis[r][c];
+static inline bool isBorder(int r, int c){
+    return r >= n || r < 0 || c >= n || c < 0 || ((vis >> (r * n + c)) & 1);
 }
 
 vector<string> a;
 
 string curr;
 void dfs(int r, int c, int d, int rem){
-    if(rem==1){
+    if(rem == 1){
         curr.push_back(dirs[d]);
         a.push_back(curr);
         curr.pop_back();
         return;
     }
-    if(r==n-1 && c==0) return;
 
-    if(rem!=n*n) curr.push_back(dirs[d]);
-    vis[r][c] = 1;
-    int ld = (d+1)%4, rd = (d+3)%4;
-    bool lb = isBorder(r+dir[ld], c+dir[ld+1]);
-    bool rb = isBorder(r+dir[rd], c+dir[rd+1]);
-    if(isBorder(r+dir[d], c+dir[d+1])){
-        if(lb && !rb) dfs(r+dir[rd], c+dir[rd+1], rd, rem-1);
-        if(rb && !lb) dfs(r+dir[ld], c+dir[ld+1], ld, rem-1);  
+    if(r == n - 1 && c == 0) return;
+
+    if(rem != n * n) curr.push_back(dirs[d]);
+
+    vis ^= 1ll << (r * n + c);
+
+    int ld = (d + 1) % 4, rd = (d + 3) % 4;
+    bool lb = isBorder(r + dir[ld], c + dir[ld + 1]);
+    bool rb = isBorder(r + dir[rd], c + dir[rd + 1]);
+
+    if(isBorder(r + dir[d], c + dir[d + 1])){
+        if(lb && !rb) dfs(r + dir[rd], c + dir[rd + 1], rd, rem - 1);
+        if(rb && !lb) dfs(r + dir[ld], c + dir[ld + 1], ld, rem - 1);  
     }
     else{
-        dfs(r+dir[d], c+dir[d+1], d, rem-1);
-        if(!rb) dfs(r+dir[rd], c+dir[rd+1], rd, rem-1);
-        if(!lb) dfs(r+dir[ld], c+dir[ld+1], ld, rem-1);
+        dfs(r + dir[d], c + dir[d + 1], d, rem - 1);
+        if(!rb) dfs(r + dir[rd], c + dir[rd + 1], rd, rem - 1);
+        if(!lb) dfs(r + dir[ld], c + dir[ld + 1], ld, rem - 1);
     }
-    vis[r][c] = 0;
-    if(rem!=n*n) curr.pop_back();
+
+    vis ^= 1ll << (r * n + c);
+
+    if(rem != n * n) curr.pop_back();
 }
 
 int main(){
@@ -52,14 +56,14 @@ int main(){
     cin>>s;
     
     auto check = [&](string &x){
-        for(int i = 0; i<48; i++){
-            if(s[i]=='?') continue;
-            if(s[i]!=x[i]) return false;
+        for(int i = 0; i < 48; i++){
+            if(s[i] == '?') continue;
+            if(s[i] != x[i]) return false;
         }
         return true;
     };
 
-    dfs(0, 0, 0, n*n);
+    dfs(0, 0, 0, n * n);
 
     int res = 0;
     for(string &t:a){
