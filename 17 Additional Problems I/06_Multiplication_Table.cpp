b8fe0c65220813035cc83b8d3dfa8fb9932eb465
Agney Komath <agneybiju@gmail.com>
2025-09-30 20:02:55 +0530

    2 More additional problems added

---
+++ 17 Additional Problems I/06_Multiplication_Table.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long n;
+    cin>>n;
+
+    long long lo = 1, hi = 1ll * n * n;
+    while(lo < hi){
+        long long mid = (lo + hi) / 2, less = 0;
+        for(int i = 1; i <= n; i++){
+            less += min(n, mid / i);
+        }
+        if(less > n * n / 2) hi = mid;
+        else lo = mid + 1;
+    }
+
+    cout<<lo;
+
+    return 0;
+}
---
+++ 17 Additional Problems I/07_Bubble_Sort_Rounds_I.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef Fusion15
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int &i:a) cin>>i;
+
+    vector<int> b(a);
+    sort(b.begin(), b.end());
+    map<int, vector<int>> mp;
+    for(int i = 0; i<n; i++) mp[b[i]].push_back(i);
+    int res = 0;
+
+    for(int i = n-1; i>=0; i--){
+        res = max(res, i - mp[a[i]].back());
+        mp[a[i]].pop_back();
+    }
+    
+    cout<<res;
+    
+    return 0;
+}
