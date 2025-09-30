73912a646776069044b21589c07efd4ab9fdd678
Agney Komath <agneybiju@gmail.com>
2025-09-30 06:05:19 +0530

    First 3 Additional Problems Added

---
+++ 17 Additional Problems I/01_Shortest_Subsequence.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    cin>>s;
+    
+    int seen = 0;
+
+    string res;
+    for(char c : s){
+        seen |= 1 << (c - 'A');
+        // 2 ^ 0 + 2 ^ 2 + 2 ^ 6 + 2 ^ 19
+        if(seen == 524357){
+            res += c;
+            seen = 0;
+        }
+    }
+
+    for(char c : "ACGT"){
+        if(!((seen >> (c - 'A')) & 1)){
+            res += c;
+            break;
+        }
+    }
+
+    cout<<res;
+    
+    return 0;
+}
---
+++ 17 Additional Problems I/03_Distinct_Values_Split.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static const int mod = 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int &i:a) cin>>i;
+
+    vector<int> b(a);
+    sort(b.begin(), b.end());
+    for(int &i:a) i = lower_bound(b.begin(), b.end(), i) - b.begin();
+
+    vector<int> dp(n + 1, 0), cnt(n, 0);
+    dp[0] = 1;
+    long long curr = 0;
+    for(int i = 0, j = 0; j<n; j++){
+        curr = (curr + dp[j]) % mod;
+        cnt[a[j]]++;
+        while(cnt[a[j]] > 1){
+            curr = (curr - dp[i] + mod) % mod;
+            --cnt[a[i++]];
+        }
+        dp[j + 1] = curr;
+    }
+
+    cout<<dp[n];
+    
+    return 0;
+}
